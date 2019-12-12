2f5521fc92f00a554f9e5952772bfe635ea72594
Sebastian Ramirez Moreno <sebramirez@iMac-de-Imac-4.local>
2019-12-12 15:57:07 -0300

    dev structure ready + model done

---
+++ school-service/src/main/java/com/people/workshop/schoolservice/models/SchoolClass.java
@@ -0,0 +1,23 @@
+package com.people.workshop.schoolservice.models;
+
+public class SchoolClass {
+
+    private int classId;
+    private String className;
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+}
---
+++ school-service/src/main/java/com/people/workshop/schoolservice/models/Student.java
@@ -0,0 +1,41 @@
+package com.people.workshop.schoolservice.models;
+
+public class Student {
+
+    private int studentId;
+    private String firstName;
+    private String lastName;
+    private int classId;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getClassId() {
+        return classId;
+    }
+
+    public void setClassId(int classId) {
+        this.classId = classId;
+    }
+}
