1567f98ea61635ca27e1839cdccfca1065897cdf
Sebastian Ramirez <s.ramirezm@icloud.com>
2019-12-12 22:42:39 -0300

    dev model's annotation added

--- school-service/src/main/java/com/people/workshop/schoolservice/models/SchoolClass.java
+++ school-service/src/main/java/com/people/workshop/schoolservice/models/SchoolClass.java
@@ -1,7 +1,15 @@
 package com.people.workshop.schoolservice.models;
 
+import org.springframework.data.annotation.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Entity
 public class SchoolClass {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int classId;
     private String className;
 
--- school-service/src/main/java/com/people/workshop/schoolservice/models/Student.java
+++ school-service/src/main/java/com/people/workshop/schoolservice/models/Student.java
@@ -1,6 +1,10 @@
 package com.people.workshop.schoolservice.models;
 
+
 import org.springframework.data.annotation.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @Entity
 public class Student {
